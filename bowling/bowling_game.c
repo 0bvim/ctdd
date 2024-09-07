190ad6663ca225015f844a8696a06e23ebe96365
nivicius <niviciusdev@gmail.com>
2024-09-07 22:00:00 +0300

    separated in directories

---
+++ bowling/bowling_game.c
@@ -0,0 +1,13 @@
+#include "bowling_game.h"
+
+static int score;
+
+void bowling_game_init() {
+  score = 0;
+}
+
+void bowling_game_roll(int pins) {
+  score += pins;
+}
+
+int bowling_game_score() { return score;}
---
+++ bowling/bowling_game.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void bowling_game_init();
+void bowling_game_roll(int pins);
+int bowling_game_score();
